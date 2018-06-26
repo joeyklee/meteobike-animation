7095ba7df0aefed0b25491728ec61e824697b631
joeyklee <j@jk-lee.com>
2018-06-25 23:55:31 -0400

    fixed order of operaitons

--- js/main.js
+++ js/main.js
@@ -7,13 +7,17 @@ window.onload = (function() {
         startBtn,
         variableSelector,
         selectedVariable,
+        campaignSelector,
+        selectedCampaign,
         legend,
         slider,
         startTime,
         endTime,
         paintStyle,
         animate,
-        activeTime;
+        activeTime,
+        time,
+        activeIds;
 
 
     /**
@@ -29,10 +33,16 @@ window.onload = (function() {
         const loadElements = function() {
             stopBtn = document.querySelector("#stop");
             startBtn = document.querySelector("#start");
+
             variableSelector = document.querySelector("#variables");
-            selectedVariable = "Temperature_diff_K";
+            selectedVariable = variableSelector.value
+
+            campaignSelector = document.querySelector("#campaign")
+            selectedCampaign = campaignSelector.value;
+
             legend = document.querySelector("#legend");
 
+            // set start and end time
             startTime = Date.parse('2018-06-14T19:00:00.000Z');
             endTime = Date.parse('2018-06-14T21:00:00.000Z');
             //  set slider props
@@ -40,7 +50,11 @@ window.onload = (function() {
             slider.setAttribute("min", startTime)
             slider.setAttribute("max", endTime)
             slider.setAttribute("step", 1000 * 60 * 2);
-            slider.setAttribute("val", startTime);
+            slider.setAttribute("value", startTime);
+
+            time = slider.value;
+
+            activeIds = [];
 
             // update text in the UI
             activeTime = document.querySelector("#active-time")
@@ -102,6 +116,65 @@ window.onload = (function() {
                 clearInterval(animate)
             });
 
+            // adjust slider time
+            slider.addEventListener('input', function(e) {
+                time = parseInt(e.target.value);
+                // update text in the UI
+                activeTime.innerText = new Date(time);
+                // change map colors
+                activeIds.forEach(id => {
+                    mapContainer.setFilter(id, ['<=', ['number', ['get', 'Time_UTC']], parseInt(time) ]);    
+                })
+                
+                // changeMapDataColors();
+            });
+
+            // adjust variables change 
+            variableSelector.addEventListener("change", (e) => {
+                selectedVariable = e.target.value;
+                console.log(selectedVariable)
+                makeLegend();
+
+                activeIds.forEach(id => {
+                    paintStyle['line-color'].property = selectedVariable;
+                    mapContainer.setPaintProperty(id, 'line-color', paintStyle['line-color']);
+                })
+                // changeMapDataColors();
+            });
+            variableSelector.dispatchEvent(new Event('change', { bubbles: false }));
+
+            // when campaign selector changes
+            // reload the main
+            campaignSelector.addEventListener("change", (e) => {
+                selectedCampaign = e.target.value;
+                console.log(selectedCampaign)
+
+                if (selectedCampaign === 'ALL-SYSTEMS-2018-06-14') {
+                    startTime = Date.parse('2018-06-14T19:00:00.000Z');
+                    endTime = Date.parse('2018-06-14T21:00:00.000Z');
+                }
+                if (selectedCampaign === 'ALL-SYSTEMS-2018-06-19') {
+                    startTime = Date.parse('2018-06-19T19:00:00.000Z');
+                    endTime = Date.parse('2018-06-19T21:00:00.000Z');
+                }
+                //  set slider props
+                time = new Date(startTime);
+                slider.setAttribute("min", startTime)
+                slider.setAttribute("max", endTime)
+                slider.setAttribute("step", 1000 * 60 * 2);
+                slider.setAttribute("value", startTime);
+                activeTime.innerText = new Date(startTime);
+
+                slider.dispatchEvent(new Event("input", {bubbles:false}));
+
+                activeIds.forEach(id => {
+                    mapContainer.removeLayer(id)
+                    mapContainer.removeSource(id)
+                })
+
+                MapApp.makeVizualization()
+            })
+
         } // end attachListeners
 
 
@@ -133,26 +206,24 @@ window.onload = (function() {
             });
         }
 
-        return {
-            init: init
+        /**
+        @ get campaign data
+        */
+        function getCampaignData() {
+            return new Promise((resolve, reject) => {
+                if (selectedCampaign !== undefined) {
+                    resolve(selectedCampaign)
+                } else {
+                    reject("no campaign selected")
+                }
+            })
         }
 
-    })();
-
-
-
-    Setup.init();
-    MapApp.init();
-
-
-
-
-    /**
-    @ Main
-    @*/
-
-    mapContainer.on('load', function() {
-        d3.csv("data/ALL-SYSTEMS-2018-06-14.csv", function(d) {
+        let loadSensorData = function(selectedCampaign){
+            /*
+            @ Parse CSV
+            @*/
+            function parseCsv(d) {
                 return {
                     System_ID: d.System_ID,
                     Time_UTC: Date.parse(d.Time_UTC),
@@ -162,81 +233,76 @@ window.onload = (function() {
                     Temperature_diff_K: +d.Temperature_diff_K,
                     Relhumidity_diff_percent: +scaleToTempDiff(+d.Relhumidity_diff_percent, -5),
                     Vapourpressure_diff_hPa: +scaleToTempDiff(+d.Vapourpressure_diff_hPa, -0.8)
-                    // Absolute_temperature_degC: d.Absolute_temperature_degC,
-                    // Relhumidity_percent: +d.Relhumidity_percent,
-                    // Vapourpressure_hPa: +d.Vapourpressure_hPa
                 };
-            })
-            .then(createLineSegments)
-            .then(addLayersToMap)
-            .then(sensorIds => {
-              // 
-              variableSelector.addEventListener("change", (e) => {
-                  let currentPalette = mapContainer.getPaintProperty("01", 'line-color');
+            }
 
-                  selectedVariable = e.target.value;
-                  // change legend
-                  legend.innerHTML = "";
-
-
-
-                  currentPalette.stops.forEach(stop => {
-                      // let legendItem = `<div class="legendItem" style="backround-color:${stop[1]}">${stop[0]}</div>`
-                      let legendItem = document.createElement("div")
-                      legendItem.classList.add('legendItem')
-                      legendItem.style.setProperty("background-color", stop[1])
-
-                      if (selectedVariable === "Temperature_diff_K") {
-                          legendItem.innerHTML = stop[0]
-                      } else if (selectedVariable === "Relhumidity_diff_percent") {
-                          legendItem.innerHTML = unscaleFromTempDiff(stop[0], -5)
-                      } else if (selectedVariable === "Vapourpressure_diff_hPa") {
-                          legendItem.innerHTML = unscaleFromTempDiff(stop[0], -0.8)
-                      } else {
-                          legendItem.innerHTML = stop[0]
-                      }
-
-                      legend.appendChild(legendItem)
-                  })
-
-                  console.log(currentPalette);
-                  sensorIds.forEach(geojson => {
-                      currentPalette.property = selectedVariable;
-                      mapContainer.setPaintProperty(geojson.id, 'line-color', currentPalette);
-                  })
-
-              })
-              variableSelector.dispatchEvent(new Event('change', { bubbles: false }));
-
-
-              slider.addEventListener('input', function(e) {
-                  let time = parseInt(e.target.value);
-                  // update the map
-                  sensorIds.forEach(geojson => {
-                      mapContainer.setFilter(geojson.id, ['<=', ['number', ['get', 'Time_UTC']], time]);
-                  })
-
-                  // update text in the UI
-                  activeTime.innerText = new Date(time);
-              });
+            
+            return new Promise( (resolve, reject) =>{
+                d3.csv(`data/${selectedCampaign}.csv`, parseCsv)
+                .then( data => {
+                    resolve(data);
+                })
             })
 
+            
+        }
 
-    }) // end map load
+        let makeVizualization = function(){
+            getCampaignData()
+                .then(loadSensorData)
+                .then(createLineSegments)
+                .then(addLayersToMap)
+                .then(applyMapStyles)
+                .then(filterMapData)
+        }
+
+
+        return {
+            init: init,
+            makeVizualization: makeVizualization
+        }
+
+    })();
+
+
+
+    Setup.init();
+    MapApp.init();
+
+    mapContainer.on('load', function() {
+        MapApp.makeVizualization();
+    })
+
+    /**
+    @  make()
+    */
+    // function make() {
+    //     // get the campaign data first
+    //     getCampaignData()
+    //         // then load up your data
+    //         .then(selectedCampaign => {
+               
+    //                 .then(createLineSegments)
+    //                 .then(addLayersToMap)
+    //                 .then(attachMapListeners)
+    //         });
+    // }
+
+
+    
 
 
     function createLineSegments(data) {
+        console.log(data.length)
         let sensorIds;
+        activeIds = [];
 
         return new Promise((resolve, reject) => {
-
 
             // filter data
             data = data.filter((d, idx) => {
                 if (idx % 2 == 0) return d;
             })
-
-            console.log(data);
 
             // get the unique sensor ids
             sensorIds = data.map(d => {
@@ -245,6 +311,9 @@ window.onload = (function() {
 
             // create a data object
             sensorIds = sensorIds.map(d => {
+                
+                activeIds.push(d);
+
                 return {
                     "type": "FeatureCollection",
                     "id": d,
@@ -253,6 +322,7 @@ window.onload = (function() {
             });
 
             variableSelector.value = selectedVariable;
+
             // fill the data in to each object
             sensorIds.forEach((geojson, idx1) => {
                 data.forEach((row, idx2) => {
@@ -276,6 +346,7 @@ window.onload = (function() {
                 })
             })
 
+            console.log("create Line Segments", sensorIds.length)
             // resolve the sensorIds as promise
             if (sensorIds) {
                 resolve(sensorIds)
@@ -288,33 +359,83 @@ window.onload = (function() {
 
     function addLayersToMap(sensorIds) {
 
-        return new Promise( (resolve, reject) => {
-          // add layer and apply style
-          sensorIds.forEach(geojson => {
-              mapContainer.addLayer({
-                  id: geojson.id,
-                  type: 'line',
-                  source: {
-                      type: 'geojson',
-                      data: geojson
-                  },
-                  filter: ['<=', ['number', ['get', 'Time_UTC']], startTime],
-                  layout: {
-                      'line-cap': 'round'
-                  },
-                  paint: paintStyle
-              })
-          })
+        console.log("addLayersToMap", sensorIds.length)
+        return new Promise((resolve, reject) => {
+            // add layer and apply style
+            sensorIds.forEach(geojson => {
+                mapContainer.addLayer({
+                    id: geojson.id,
+                    type: 'line',
+                    source: {
+                        type: 'geojson',
+                        data: geojson
+                    },
+                    filter: ['<=', ['number', ['get', 'Time_UTC']], startTime],
+                    layout: {
+                        'line-cap': 'round'
+                    },
+                    paint: paintStyle
+                })
+            })
 
-          // pass data along
-          if(sensorIds){
-            resolve(sensorIds);  
-          } else{
-            reject("error in addLayersToMap")
-          }
+            // pass data along
+            if (sensorIds) {
+                resolve(sensorIds);
+            } else {
+                reject("error in addLayersToMap")
+            }
         })
     };
 
+    function makeLegend(){
+
+      // change legend
+      legend.innerHTML = "";
+
+      paintStyle['line-color'].stops.forEach(stop => {
+          let legendItem = document.createElement("div")
+          legendItem.classList.add('legendItem')
+          legendItem.style.setProperty("background-color", stop[1])
+
+          if (selectedVariable === "Temperature_diff_K") {
+              legendItem.innerHTML = stop[0]
+          } else if (selectedVariable === "Relhumidity_diff_percent") {
+              legendItem.innerHTML = unscaleFromTempDiff(stop[0], -5)
+          } else if (selectedVariable === "Vapourpressure_diff_hPa") {
+              legendItem.innerHTML = unscaleFromTempDiff(stop[0], -0.8)
+          } else {
+              legendItem.innerHTML = stop[0]
+          }
+
+          legend.appendChild(legendItem)
+      })
+
+    }
+
+
+    function applyMapStyles(sensorIds){ 
+        console.log(sensorIds)
+        sensorIds.forEach(geojson => {
+            paintStyle['line-color'].property = selectedVariable;
+            mapContainer.setPaintProperty(geojson.id, 'line-color', paintStyle['line-color']);
+        })
+
+        return new Promise( (resolve, reject) => {
+          resolve(sensorIds);
+        })
+      
+    } 
+
+
+            
+
+
+    function filterMapData(sensorIds){
+      // update the map
+      sensorIds.forEach(geojson => {
+          mapContainer.setFilter(geojson.id, ['<=', ['number', ['get', 'Time_UTC']], parseInt(time) ]);
+      })
+    }
 
     /**
     @ helper functions
